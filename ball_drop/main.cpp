51c3d02144c9600b5a18bdae92a6a7d276dcbfe5
Timothy Liu <timsgithub@gmail.com>
2019-05-20 09:21:23 -0700

    Print ball height until reach ground and validate user input for tower height

--- ball_drop/main.cpp
+++ ball_drop/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <limits>
 #include "constants.hpp"
 
 double getTowerHeight();
 double getDistFallen(int);
 double getBallHeight(double h, int t);
-void printBallHeight(double h, int t);
+void printBallHeight(double h);
 
 int main()
 {
@@ -12,7 +13,7 @@ int main()
     double height{ getTowerHeight() };
 
     // Output height in 1-second intervals over 5 seconds
-    printBallHeight(height, 5);
+    printBallHeight(height);
 
     return 0;
 }
@@ -22,6 +23,15 @@ double getTowerHeight()
     std::cout << "Enter the height of the tower in meters: ";
     double userInput{};
     std::cin >> userInput;
+    
+    // Check if extraction failed
+    while (std::cin.fail())
+    {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Invalid input. Try again: ";
+        std::cin >> userInput;
+    }
     return userInput;
 }
 
@@ -35,18 +45,16 @@ double getBallHeight(double h, int t)
     return h - getDistFallen(t);
 }
 
-void printBallHeight(double h, int t)
+void printBallHeight(double h)
 {
-    for (int i = 0; i <= t; i++)
+    int time = 0;
+    double curHeight { getBallHeight(h, time) };
+    while (curHeight >= 0)
     {
-        std::cout << "At " << i << " seconds, the ball is ";
-
-        double curHeight{ getBallHeight(h, i) };
-
-        // Check if ball has reached the ground
-        if (curHeight <= 0)
-            std::cout << "on the ground." << std::endl;
-        else
-            std::cout << "at height " << curHeight << " meters" << std::endl;
+        std::cout << "At " << time << " seconds, the ball is at height "
+            << curHeight << " meters" << std::endl;
+        time++;
+        curHeight = getBallHeight(h, time);
     }
+    std::cout << "At " << time << " seconds, the ball is on the ground.\n";
 }
